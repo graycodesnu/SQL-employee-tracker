b5b12a2767191270a1b57ac7772c1bdd24e4ddc7
Grayson Harvey <graycodes.nu@gmail.com>
2022-10-16 21:16:42 -0500

    finally worked through seed data issues and am able to seed the db using source seeds.sql.

---
+++ seeds.sql
@@ -0,0 +1,35 @@
+INSERT INTO department (name) 
+VALUES 
+  ('Legal'),
+  ('Engineering'),
+  ('Catering'),
+  ('Recreation'),
+  ('Human Resources');
+
+INSERT INTO role 
+  (title, salary, department_id)
+VALUES 
+  ('Family Lawyer', 5, 1),
+  ('Software Engineer', 3, 2),
+  ('Expert Chef', 100, 3),
+  ('Professional Sunbather', 500, 4),
+  ('Expert Goofball', 100, 5);
+
+INSERT INTO employee
+  (first_name, last_name, role_id, manager_id)
+VALUES 
+  ('Ana', 'Garduno', 1, NULL),
+  ('Grayson', 'Harvey', 2, NULL), 
+  ('Malcolm', 'Harvey', 3, NULL),
+  ('Alfie', 'Harvey', 4, NULL),
+  ('Frankie', 'Harvey', 5, NULL),
+  ('Lightning', 'McQueen', 1, 1),
+  ('Tow', 'Mater', 1, 1),
+  ('Beckett', 'Costello', 2, 1),
+  ('Susie', 'Sorensen', 2, 2),
+  ('Stinky', 'Jones', 3, 2),
+  ('Guy', 'Fieri', 4, 3), 
+  ('Fred', 'Flinstone', 4, 4),
+  ('Clayton', 'Kershaw', 5, 5),
+  ('Mookie', 'Betts', 4, 5);
+
